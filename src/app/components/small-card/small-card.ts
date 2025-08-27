861f1f9cffd45ea029a1722acd44c04d7064d29f
Ryan Ferreira <ryanfsilva6@gmail.com>
2025-08-27 11:50:12 -0300

    components created with styles

---
+++ src/app/components/small-card/small-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-small-card',
+  imports: [],
+  templateUrl: './small-card.html',
+  styleUrl: './small-card.css'
+})
+export class SmallCard {
+
+}
